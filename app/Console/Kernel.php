d72094da7f4d6f95f07a838d7e8c31976bc97cd1
Leorio Crypto <le0xcrypto@gmail.com>
2021-07-15 14:46:32 +0200

    Expose address api

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('apple:fetch:new')->everyMinute();
+        $schedule->command('apple:fetch:new')->everyFiveMinutes();
     }
 
     /**
